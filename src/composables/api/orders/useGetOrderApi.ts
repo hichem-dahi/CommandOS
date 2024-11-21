367e92a15204be427d0d6e54290a9cb27cc5897f
Hichem <hichemdahi57@gmail.com>
2024-11-21 22:08:30 +0100

    refactor: useGetOrderApi data types

--- src/composables/api/orders/useGetOrderApi.ts
+++ src/composables/api/orders/useGetOrderApi.ts
@@ -3,35 +3,33 @@ import { useAsyncState } from '@vueuse/core'
 import { supabase } from '@/supabase/supabase'
 
 // Define types outside the function for reuse
-type QueryResult = Awaited<ReturnType<typeof fetchOrder>>
-export type OrderData = NonNullable<QueryResult['data']>
+export type OrderData = NonNullable<ReturnType<typeof useGetOrderApi>['data']['value']>
 export type OrderLineData = OrderData['order_lines'][0]
-
-// Function to perform the Supabase query
-async function fetchOrder(orderId: string) {
-  return supabase
-    .from('orders')
-    .select(
-      `*,
-        client:organizations!orders_client_id_fkey (*),    
-        order_lines:order_lines (
-          *,
-          product:products (*)
-        ),    
-        payments(*),
-        delivery:deliveries (*),
-        individual:individuals (*)
-      `
-    )
-    .eq('id', orderId)
-    .single()
-}
 
 // Composition function to fetch an order
 export function useGetOrderApi() {
-  const orderId = ref<string | undefined>()
+  const orderId = ref<string>()
 
-  const query = async () => (orderId.value ? fetchOrder(orderId.value) : undefined)
+  const query = async () =>
+    orderId.value
+      ? supabase
+          .from('orders')
+          .select(
+            `
+              *,
+              client:organizations!orders_client_id_fkey (*),    
+              order_lines:order_lines (
+                *,
+                product:products (*)
+              ),    
+              payments(*),
+              delivery:deliveries (*),
+              individual:individuals (*)
+            `
+          )
+          .eq('id', orderId.value)
+          .single()
+      : undefined
 
   const q = useAsyncState(query, undefined, { immediate: false })
 
