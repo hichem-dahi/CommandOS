b28ec220a64b4ddee9d64b768b73cc527dfafe4e
Hichem <hichemdahi57@gmail.com>
2024-11-24 20:51:02 +0100

    feat: add service worker manually

---
+++ src/service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {}
+
+  const options = {
+    body: data.body,
+    icon: '/icon-192x192.png',
+    badge: '/icon-192x192.png',
+    data: {
+      url: data.url || '/'
+    }
+  }
+
+  event.waitUntil(self.registration.showNotification(data.title, options))
+})
