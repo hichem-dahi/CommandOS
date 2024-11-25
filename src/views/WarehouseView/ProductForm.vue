f0c650b00e63b8d53b9775a1f52f91753ca61253
Hichem <hichemdahi57@gmail.com>
2024-11-25 14:09:34 +0100

    feat: use org_id on productForm

--- src/views/WarehouseView/ProductForm.vue
+++ src/views/WarehouseView/ProductForm.vue
@@ -87,7 +87,7 @@ const model = defineModel({
   default: {
     code: '',
     name: '',
-    organization_id: self.value.user?.organization_id || '',
+    org_id: self.value.user?.organization_id || '',
     qte: 0,
     price: 0,
     cost_price: null as number | null,
@@ -98,7 +98,7 @@ const model = defineModel({
 const rules = {
   code: { required },
   name: { required },
-  organization_id: { required },
+  org_id: { required },
   qte: { required, numeric, minValue: minValue(1) },
   price: { required, numeric, minValue: minValue(1) },
   cost_price: { numeric, minValue: minValue(1) }
