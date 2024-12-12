df7db95616ec9c34066ca13b7406e81159e2b6a2
Hichem <hichemdahi57@gmail.com>
2024-12-12 13:42:11 +0100

    refactor: add multiple rows && remove onSuccess

--- src/composables/api/notifications/useInsertNotificationApi.ts
+++ src/composables/api/notifications/useInsertNotificationApi.ts
@@ -1,34 +1,26 @@
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { useAsyncState } from '@vueuse/core'
 
 import { supabase } from '@/supabase/supabase'
 
 import type { TablesInsert } from '@/types/database.types'
 
-export function useInsertNotificationApi() {
-  const form = ref<TablesInsert<'notifications'>>()
+export function useUpsertNotificationsApi() {
+  const form = ref<TablesInsert<'notifications'>[]>()
 
   const query = async () => {
-    if (form.value) {
-      return supabase.from('notifications').insert(form.value).select().single()
+    if (form.value?.length) {
+      return supabase.from('notifications').insert(form.value).select()
     } else {
       throw new Error('Form is null or incomplete')
     }
   }
 
-  const q = useAsyncState(query, undefined) // Invoke query properly
+  const q = useAsyncState(query, undefined, { immediate: false }) // Invoke query properly
 
   const data = computed(() => q.state.value?.data)
   const error = computed(() => q.state.value?.error)
   const isSuccess = computed(() => q.isReady.value && !error.value)
 
-  const onSuccess = (fn: () => void) => {
-    watch(isSuccess, (newValue) => {
-      if (newValue) {
-        fn()
-      }
-    })
-  }
-
-  return { ...q, data, error, isSuccess, form, onSuccess }
+  return { ...q, data, error, isSuccess, form }
 }
