5f6a3b37aa3a0bf27b1704c7e5b300a8a9d4b2c0
Hichem <hichemdahi57@gmail.com>
2024-12-13 23:25:18 +0100

    feat: add upsertStockMovementsApi

---
+++ src/composables/api/stockMovements/useUpsertStockMovementsApi.ts
@@ -0,0 +1,26 @@
+import { computed, ref } from 'vue'
+import { useAsyncState } from '@vueuse/core'
+
+import { supabase } from '@/supabase/supabase'
+
+import type { TablesInsert } from '@/types/database.types'
+
+export function useUpsertStockMovementsApi() {
+  const form = ref<TablesInsert<'stock_movements'>[]>() // Use ref to make it reactive
+
+  const query = async () => {
+    if (form.value?.length) {
+      return supabase.from('stock_movements').upsert(form.value).select()
+    } else {
+      return undefined
+    }
+  }
+
+  const q = useAsyncState(query, undefined) // Invoke query properly
+
+  const data = computed(() => q.state.value?.data)
+  const error = computed(() => q.state.value?.error)
+  const isSuccess = computed(() => q.isReady.value && !error.value)
+
+  return { ...q, data, error, isSuccess, form }
+}
