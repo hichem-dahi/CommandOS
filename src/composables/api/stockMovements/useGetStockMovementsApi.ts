4fe6a36ef9d93ba0c72b7e8fbd7ad7294bb3e92e
Hichem <hichemdahi57@gmail.com>
2024-12-13 23:24:09 +0100

    feat: add date filter to getStockMovementsApi

--- src/composables/api/stockMovements/useGetStockMovementsApi.ts
+++ src/composables/api/stockMovements/useGetStockMovementsApi.ts
@@ -1,24 +1,34 @@
-import { computed, ref } from 'vue'
+import { computed, reactive } from 'vue'
 import { useAsyncState } from '@vueuse/core'
 
 import { supabase } from '@/supabase/supabase'
 
+import self from '@/composables/localStore/useSelf'
+
+const orgId = self.value.user?.organization_id
+
 export function useGetStockMovementsApi() {
-  const productId = ref<string>() // Use ref to make it reactive
+  const params = reactive({
+    date: ''
+  }) // Use ref to make it reactive
 
   const query = async () => {
-    if (productId.value) {
-      return supabase.from('stock_movements').select().eq('product_id', productId.value)
-    } else {
-      throw new Error('Form is null or incomplete')
+    if (orgId) {
+      const query = supabase.from('stock_movements').select().eq('org_id', orgId)
+
+      if (params.date) {
+        query.gt('updated_at', params.date)
+      }
+      return query
     }
+    return undefined
   }
 
-  const q = useAsyncState(query, undefined) // Invoke query properly
+  const q = useAsyncState(query, undefined, { immediate: false }) // Invoke query properly
 
   const data = computed(() => q.state.value?.data)
   const error = computed(() => q.state.value?.error)
-  const isSuccess = computed(() => q.isReady.value && q.state.value?.statusText === 'OK')
+  const isSuccess = computed(() => q.isReady.value && !error.value)
 
-  return { ...q, data, error, isSuccess, productId }
+  return { ...q, data, error, isSuccess, params }
 }
