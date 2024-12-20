095b3a63166f5bd0346980881c25e86cd3d46c84
Hichem <hichemdahi57@gmail.com>
2024-12-20 18:18:57 +0100

    feat: cache routes in service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,10 @@
-import { precacheAndRoute } from 'workbox-precaching'
+import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
 
 precacheAndRoute(self.__WB_MANIFEST)
+
+// to allow work offline
+registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
 
 self.addEventListener('push', (event) => {
   const data = event.data ? event.data.json() : {}
