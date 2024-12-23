ceffd028230ebe7cbf42cfc7e04e259142527a3f
Hichem <hichemdahi57@gmail.com>
2024-12-23 01:07:23 +0100

    feat: add migration (delivery.org_id field & individual. phone not nul)

--- supabase/migrations/20241223000528_add_org_id_field_to_deliveries_table_make_phone_not_optionnel.sql
+++ supabase/migrations/20241223000528_add_org_id_field_to_deliveries_table_make_phone_not_optionnel.sql
@@ -1,5 +1,7 @@
 alter table "public"."deliveries" add column "org_id" uuid not null;
 
+alter table "public"."individuals" alter column "phone" set not null;
+
 alter table "public"."deliveries" add constraint "deliveries_org_id_fkey" FOREIGN KEY (org_id) REFERENCES organizations(id) not valid;
 
 alter table "public"."deliveries" validate constraint "deliveries_org_id_fkey";
