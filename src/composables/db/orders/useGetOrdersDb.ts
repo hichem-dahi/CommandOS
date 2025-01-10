bc697276eeb08c053b2f838a30489fdf244eed11
Hichem <hichemdahi57@gmail.com>
2025-01-10 19:52:08 +0100

    feat: sort orders by date

--- src/composables/db/orders/useGetOrdersDb.ts
+++ src/composables/db/orders/useGetOrdersDb.ts
@@ -1,8 +1,6 @@
 import { computed, reactive, ref } from 'vue'
 import { useLiveQuery } from '@electric-sql/pglite-vue'
-
 import self from '@/composables/localStore/useSelf'
-
 import type { Tables } from '@/types/database.types'
 import type { ProductData } from '../products/useGetProductsDb'
 
@@ -57,68 +55,72 @@ export function useOrdersQuery() {
 
     return isReady.value
       ? `
-    SELECT
-      o.*,
-      (
-          SELECT to_jsonb(i)
-          FROM public.individuals i
-          WHERE i.id = o.individual_id AND i._deleted = false
-          LIMIT 1
-      ) AS individual,
-      (
-          SELECT to_jsonb(org)
-          FROM public.organizations org
-          WHERE org.id = o.client_id AND org._deleted = false
-          LIMIT 1
-      ) AS client,
-      (
-          SELECT jsonb_agg(p)
-          FROM public.payments p
-          WHERE p.order_id = o.id AND p._deleted = false
-      ) AS payments,
-      (
-          SELECT jsonb_agg(
-              jsonb_build_object(
-                  'id', ol.id,
-                  'order_id', ol.order_id,
-                  'product_id', ol.product_id,
-                  'org_id', ol.org_id,
-                  'qte', ol.qte,
-                  'unit_price', ol.unit_price,
-                  'unit_cost_price', ol.unit_cost_price,
-                  'total_price', ol.total_price,
-                  'product', (
-                    SELECT jsonb_build_object(
-                      'id', p.id,
-                      'code', p.code,
-                      'name', p.name,
-                      'price', p.price,
-                      'cost_price', p.cost_price,
-                      'qty', COALESCE(pq.qty, 0),
-                      'org_id', p.org_id,
-                      'bar_code', p.bar_code,
-                      'updated_at', p.updated_at,
-                      '_synced', p._synced,
-                      '_deleted', p._deleted
-                    )
-                    FROM public.products p
-                    LEFT JOIN public.products_qty pq ON pq.id = p.id
-                    WHERE p.id = ol.product_id AND p._deleted = false
-                    LIMIT 1
+    WITH ordered_orders AS (
+      SELECT
+        o.*,
+        (
+            SELECT to_jsonb(i)
+            FROM public.individuals i
+            WHERE i.id = o.individual_id AND i._deleted = false
+            LIMIT 1
+        ) AS individual,
+        (
+            SELECT to_jsonb(org)
+            FROM public.organizations org
+            WHERE org.id = o.client_id AND org._deleted = false
+            LIMIT 1
+        ) AS client,
+        (
+            SELECT jsonb_agg(p)
+            FROM public.payments p
+            WHERE p.order_id = o.id AND p._deleted = false
+        ) AS payments,
+        (
+            SELECT jsonb_agg(
+                jsonb_build_object(
+                    'id', ol.id,
+                    'order_id', ol.order_id,
+                    'product_id', ol.product_id,
+                    'org_id', ol.org_id,
+                    'qte', ol.qte,
+                    'unit_price', ol.unit_price,
+                    'unit_cost_price', ol.unit_cost_price,
+                    'total_price', ol.total_price,
+                    'product', (
+                      SELECT jsonb_build_object(
+                        'id', p.id,
+                        'code', p.code,
+                        'name', p.name,
+                        'price', p.price,
+                        'cost_price', p.cost_price,
+                        'qty', COALESCE(pq.qty, 0),
+                        'org_id', p.org_id,
+                        'bar_code', p.bar_code,
+                        'updated_at', p.updated_at,
+                        '_synced', p._synced,
+                        '_deleted', p._deleted
+                      )
+                      FROM public.products p
+                      LEFT JOIN public.products_qty pq ON pq.id = p.id
+                      WHERE p.id = ol.product_id AND p._deleted = false
+                      LIMIT 1
+                  )
                 )
-              )
-          )
-          FROM public.order_lines ol
-          WHERE ol.order_id = o.id AND ol._deleted = false
-      ) AS order_lines,
-      (
-          SELECT to_jsonb(d)
-          FROM public.deliveries d
-          WHERE d.id = o.delivery_id AND d._deleted = false
-          LIMIT 1
-      ) AS delivery
-    FROM public.orders o
-    ${queryConditions};
+            )
+            FROM public.order_lines ol
+            WHERE ol.order_id = o.id AND ol._deleted = false
+        ) AS order_lines,
+        (
+            SELECT to_jsonb(d)
+            FROM public.deliveries d
+            WHERE d.id = o.delivery_id AND d._deleted = false
+            LIMIT 1
+        ) AS delivery
+      FROM public.orders o
+      ${queryConditions}
+    )
+    SELECT * FROM ordered_orders
+    ORDER BY date DESC NULLS LAST;
     `
       : ''
   })
