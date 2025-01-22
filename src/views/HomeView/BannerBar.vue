0a4e777bc6b6a15b8c6fe6805c24831308cac88e
Hichem <hichemdahi57@gmail.com>
2025-01-22 11:55:06 +0100

    fix: maximize install app banner

--- src/views/HomeView/BannerBar.vue
+++ src/views/HomeView/BannerBar.vue
@@ -4,7 +4,7 @@
     class="text-left"
     position="fixed"
     bg-color="blue"
-    style="bottom: 0; z-index: 1000"
+    style="bottom: 0; z-index: 10000"
   >
     Get our free app. It won't take up space on your phone and also works offline!
     <template v-slot:actions>
