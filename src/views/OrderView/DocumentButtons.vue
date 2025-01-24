a45bf6878e7f49f4088e225dc0ee763d1ce7b220
Hichem <hichemdahi57@gmail.com>
2025-01-24 14:42:28 +0100

    refactor: DocumentButtons

--- src/views/OrderView/DocumentButtons.vue
+++ src/views/OrderView/DocumentButtons.vue
@@ -1,26 +1,21 @@
 <template>
-  <template v-for="(docType, index) in documentTypes" :key="index">
-    <v-btn
-      variant="text"
-      :prepend-icon="docType.icon"
-      :disabled="disabled"
-      @click="goDocPage"
-      target="_blank"
-      :text="$t(docType.label)"
-      v-if="order?.document_type === docType.type"
-    />
-  </template>
+  <v-btn
+    ref="bt"
+    variant="text"
+    :prepend-icon="docType.icon"
+    :disabled="disabled"
+    @click="goDocPage"
+    target="_blank"
+    :text="$t(docType.label)"
+  />
 </template>
 
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue'
 import { mdiInvoice, mdiReceiptText, mdiTruckCheck, mdiNote } from '@mdi/js'
-import { DocumentType } from '@/models/models'
 
 import type { OrderData } from '@/composables/db/orders/useGetOrdersDb'
-
-defineProps<{ readonly order: OrderData; disabled: boolean }>()
-
-const emit = defineEmits(['go-doc-page'])
+import { DocumentType } from '@/models/models'
 
 const documentTypes = [
   { type: DocumentType.Invoice, icon: mdiReceiptText, label: 'invoice' },
@@ -28,6 +23,16 @@ const documentTypes = [
   { type: DocumentType.Voucher, icon: mdiInvoice, label: 'voucher' },
   { type: DocumentType.Proforma, icon: mdiNote, label: 'proforma' }
 ]
+
+const props = defineProps<{ readonly order: OrderData; disabled: boolean }>()
+
+const emit = defineEmits(['go-doc-page'])
+
+const docType = ref(documentTypes.find((d) => props.order.doc_index == d.type))
+
+watchEffect(() => {
+  docType.value = documentTypes.find((d) => props.order.document_type == d.type)
+})
 
 function goDocPage() {
   emit('go-doc-page')
