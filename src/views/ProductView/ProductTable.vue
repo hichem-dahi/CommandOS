24d916c65ea3391241d74efe47cb4f1670d7cd05
Hichem <hichemdahi57@gmail.com>
2025-11-27 18:31:18 +0100

    feat: restyle ProductTable

--- src/views/ProductView/ProductTable.vue
+++ src/views/ProductView/ProductTable.vue
@@ -1,62 +1,109 @@
 <!-- eslint-disable vue/valid-v-slot -->
 <template>
-  <v-data-table v-if="product" :items="items" :headers="headers" style="background-color: #fefefe">
-    <template v-slot:top>
-      <v-card class="pb-2 d-flex align-end" color="#F7F7F7" elevation="0">
-        <div class="col-1 d-flex justify-space-between w-75">
+  <v-data-table
+    v-if="product"
+    :items="items"
+    :headers="headers"
+    density="comfortable"
+    hover
+    class="bg-white rounded-lg"
+  >
+    <template #top>
+      <v-card
+        color="#F7F7F7"
+        elevation="0"
+        class="px-4 py-3 d-flex align-center justify-space-between"
+      >
+        <div class="d-flex align-center gap-4">
+          <v-avatar size="64" color="grey-lighten-3">
+            <v-icon size="32" color="grey" :icon="mdiPackageVariant"></v-icon>
+          </v-avatar>
+
           <div>
-            <v-card-title>
-              <div>{{ product?.name }}</div>
+            <v-card-title class="text-h6 font-weight-medium mb-1">
+              {{ product?.name }}
             </v-card-title>
             <v-card-subtitle class="text-body-2">
-              <span>{{ $t('code') }}:</span>
-              <span>&nbsp;{{ product?.code }}</span> <br />
-
-              <span>{{ $t('U.P') }}:</span>
-              <span>&nbsp;{{ product?.price }} DA</span> <br />
-
-              <div v-if="product?.cost_price">
-                <span>{{ $t('C.P') }}:</span>
-                <span>&nbsp;{{ product?.cost_price }} DA</span>
+              <div class="d-flex flex-wrap align-center ga-2">
+                <v-chip variant="outlined" rounded="xl" size="small" label>
+                  {{ $t('code') }}: {{ product?.code }}
+                </v-chip>
+                <v-chip variant="outlined" rounded="xl" size="small" label>
+                  {{ $t('U.P') }}: {{ product?.price }} DA
+                </v-chip>
+                <v-chip
+                  variant="outlined"
+                  rounded="xl"
+                  v-if="product?.cost_price"
+                  size="small"
+                  label
+                >
+                  {{ $t('C.P') }}: {{ product?.cost_price }} DA
+                </v-chip>
+                <v-chip variant="outlined" rounded="xl" size="small" label>
+                  {{ $t('quantity') }}: {{ product?.qty }}
+                </v-chip>
               </div>
-
-              <span>{{ $t('quantity') }}:</span>
-              <span>&nbsp;{{ product?.qty }}</span> <br />
             </v-card-subtitle>
           </div>
         </div>
-        <div class="col-2">
-          <v-btn variant="text" color="primary" size="small" @click="modifyStockDialog = true">
-            {{ $t('modify-stock') }}
-          </v-btn>
-          <ModifyStock v-model="modifyStockDialog" :product="product" />
-        </div>
+
+        <v-tooltip text="Modify product stock" location="bottom">
+          <template #activator="{ props }">
+            <v-btn
+              v-bind="props"
+              color="primary"
+              size="small"
+              prepend-icon="mdi-pencil"
+              @click="modifyStockDialog = true"
+            >
+              {{ $t('modify-stock') }}
+            </v-btn>
+          </template>
+        </v-tooltip>
+
+        <ModifyStock v-model="modifyStockDialog" :product="product" />
       </v-card>
+
+      <v-divider class="my-2" />
     </template>
-    <template v-slot:item.order="{ item }">
-      <v-btn
-        v-if="item.order"
-        variant="text"
-        size="small"
-        color="light-blue"
-        :icon="mdiOpenInNew"
-        :to="{ name: 'order', params: { order_id: item.order } }"
-      >
-      </v-btn>
+
+    <template #item.order="{ item }">
+      <v-tooltip text="View order">
+        <template #activator="{ props }">
+          <v-btn
+            v-if="item.order"
+            v-bind="props"
+            variant="text"
+            size="small"
+            color="primary"
+            :to="{ name: 'order', params: { order_id: item.order } }"
+            class="text-capitalize"
+          >
+            <v-icon :icon="mdiOpenInNew" size="18" />
+          </v-btn>
+        </template>
+      </v-tooltip>
     </template>
-    <template v-slot:item.date="{ item }">
-      <div class="text-no-wrap">{{ format(item.date, 'yyyy-MM-dd') }}</div>
-      <div class="text-no-wrap">{{ format(item.date, 'p') }}</div>
+
+    <template #item.date="{ item }">
+      <div class="text-no-wrap">
+        {{ format(item.date, 'yyyy-MM-dd') }}
+      </div>
+      <div class="text-no-wrap text-grey text-caption">
+        {{ format(item.date, 'p') }}
+      </div>
     </template>
   </v-data-table>
 </template>
+
 <script setup lang="ts">
 import { ref, computed, toRef } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { format } from 'date-fns'
 import { sortBy } from 'lodash'
 import { useLiveQuery } from '@electric-sql/pglite-vue'
-import { mdiOpenInNew } from '@mdi/js'
+import { mdiOpenInNew, mdiPackageVariant } from '@mdi/js'
 
 import ModifyStock from './ModifyStock.vue'
 
