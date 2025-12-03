c14625ecd39873552a62838b70350773aa0280ec
Hichem <hichemdahi57@gmail.com>
2025-12-03 12:05:09 +0100

    feat: add to orders filters status param

--- src/views/OrdersView/models/models.ts
+++ src/views/OrdersView/models/models.ts
@@ -1,6 +1,7 @@
-import type { DocumentType } from '@/models/models'
+import type { DocumentType, OrderStatus } from '@/models/models'
 
 export interface Filters {
   docType: DocumentType | null
   dateRange: Date[] | string[]
+  status: OrderStatus | null
 }
