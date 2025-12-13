04e71062d91c203482c8e02ac169155b4e8a72f1
Hichem Dahi <hichemdahi57@gmail.com>
2025-12-13 17:18:35 +0100

    feat: add white bg for add sale view view

--- src/views/CreateSaleView.vue
+++ src/views/CreateSaleView.vue
@@ -1,193 +1,188 @@
 <template>
-  <div class="text-h4 py-8 px-4">
-    {{ $t('add-sale') }}
-  </div>
-  <v-row>
-    <v-col>
-      <v-sheet class="d-flex align-end pa-4 ga-6 flex-wrap" rounded="lg" color="#F6F6F5">
-        <!-- Title -->
-        <div class="text-h6 font-weight-medium text-capitalize">{{ $t('options') }}:</div>
+  <v-card class="px-8 py-4" elevation="2">
+    <div class="text-h4 py-8 px-4">
+      {{ $t('add-sale') }}
+    </div>
+    <v-row>
+      <v-col>
+        <v-sheet class="d-flex align-end pa-4 ga-6 flex-wrap" rounded="lg">
+          <!-- Title -->
+          <div class="text-h6 font-weight-medium text-capitalize">{{ $t('options') }}:</div>
 
-        <!-- Consumer Selector -->
-        <div class="d-flex align-center ga-3">
+          <!-- Consumer Selector -->
+          <div class="d-flex align-center ga-3">
+            <v-btn
+              v-if="!consumerPicked"
+              variant="tonal"
+              color="primary"
+              @click="clientDialog = true"
+            >
+              {{ $t('add-client') }}
+            </v-btn>
+
+            <v-chip
+              v-else
+              closable
+              size="small"
+              color="primary"
+              variant="flat"
+              @click:close="resetClient"
+            >
+              {{ $t('client') }}:
+              {{ form.individual_id ? individualForm.name : clientForm.name }}
+            </v-chip>
+
+            <!-- Dialog -->
+            <v-dialog v-model="clientDialog" max-width="500">
+              <CreateOrderStepper @success="clientDialog = false" />
+            </v-dialog>
+          </div>
+
+          <!-- Divider between sections -->
+          <v-divider vertical class="mx-2" />
+
+          <!-- Transaction Type Radios -->
+          <v-radio-group v-model="form.type" hide-details inline class="d-flex align-center">
+            <v-radio :label="$t('sale')" value="sale" />
+
+            <v-tooltip :disabled="!!consumerPicked" text="Select a consumer first">
+              <template #activator="{ props }">
+                <v-radio
+                  v-bind="props"
+                  :label="$t('order')"
+                  value="order"
+                  :disabled="!consumerPicked"
+                />
+              </template>
+            </v-tooltip>
+          </v-radio-group>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-divider />
+    <v-row no-gutters>
+      <v-col cols="12" md="7" align-self="center">
+        <CreateOrderlines />
+      </v-col>
+      <v-divider class="mx-auto" vertical />
+      <!-- RIGHT SUMMARY -->
+      <v-col cols="12" md="4">
+        <v-card variant="text" class="pa-4">
+          <v-card-subtitle class="text-body-1 mb-1">
+            {{ $t('total') }}
+          </v-card-subtitle>
+
+          <v-card-title class="text-h4 mb-4">
+            <span class="text-primary">{{ form.total_price }}</span> {{ $t('DA') }}
+          </v-card-title>
+
+          <v-card-text class="d-flex flex-column">
+            <v-number-input
+              class="orderline-input"
+              density="compact"
+              max-width="160"
+              variant="underlined"
+              :label="$t('payment')"
+              :max="form.total_price"
+              v-model="paymentForm.amount"
+            />
+
+            <v-number-input
+              class="orderline-input"
+              density="compact"
+              max-width="160"
+              variant="underlined"
+              :label="$t('reduction')"
+              :max="form.total_price"
+              v-model="form.reduction"
+            />
+            <v-chip v-if="toPay > 0" variant="text" size="small" color="warning">
+              {{ $t('remaining') }}: {{ toPay }} {{ $t('DA') }}
+            </v-chip>
+            <v-divider class="my-4"></v-divider>
+          </v-card-text>
+
+          <v-card-actions class="flex-column justify-end ga-2">
+            <v-btn
+              color="primary"
+              variant="flat"
+              rounded
+              block
+              @click="submitSale"
+              :loading="isLoading"
+            >
+              {{ $t('confirm') }}
+            </v-btn>
+
+            <v-btn color="secondary" variant="tonal" rounded block @click="handleReset">
+              {{ $t('reset') }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col v-if="showScanner" cols="12">
+        <v-card>
+          <v-card-text>
+            <BarcodeScanner @detected="(v) => selectProduct(v, orderlinesForm)" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <!-- FOOTER ACTIONS -->
+        <div class="d-flex justify-space-between">
+          <div class="d-flex ga-2 flex-wrap">
+            <v-chip
+              v-for="(_, i) in savedSales"
+              :key="i"
+              @click="restoreSale(i)"
+              size="small"
+              color="secondary"
+              variant="tonal"
+            >
+              {{ $t('sale') }} #{{ i }}
+            </v-chip>
+          </div>
           <v-btn
-            v-if="!consumerPicked"
-            variant="tonal"
-            size="small"
-            color="primary"
-            @click="clientDialog = true"
-          >
-            {{ $t('add-client') }}
-          </v-btn>
-
-          <v-chip
-            v-else
-            closable
-            size="small"
-            color="primary"
-            variant="flat"
-            @click:close="resetClient"
-          >
-            {{ $t('client') }}:
-            {{ form.individual_id ? individualForm.name : clientForm.name }}
-          </v-chip>
-
-          <!-- Dialog -->
-          <v-dialog v-model="clientDialog" max-width="500">
-            <CreateOrderStepper @success="clientDialog = false" />
-          </v-dialog>
-        </div>
-
-        <!-- Divider between sections -->
-        <v-divider vertical class="mx-2" />
-
-        <!-- Transaction Type Radios -->
-        <v-radio-group
-          density="compact"
-          v-model="form.type"
-          hide-details
-          inline
-          class="d-flex align-center"
-        >
-          <v-radio :label="$t('sale')" value="sale" />
-
-          <v-tooltip :disabled="!!consumerPicked" text="Select a consumer first">
-            <template #activator="{ props }">
-              <v-radio
-                v-bind="props"
-                :label="$t('order')"
-                value="order"
-                :disabled="!consumerPicked"
-              />
-            </template>
-          </v-tooltip>
-        </v-radio-group>
-      </v-sheet>
-    </v-col>
-  </v-row>
-  <v-divider />
-  <v-row no-gutters>
-    <v-col cols="12" md="7" align-self="center">
-      <CreateOrderlines />
-    </v-col>
-    <v-divider class="mx-auto" vertical />
-    <!-- RIGHT SUMMARY -->
-    <v-col cols="12" md="4">
-      <v-card variant="text" class="pa-4">
-        <v-card-subtitle class="text-body-1 mb-1">
-          {{ $t('total') }}
-        </v-card-subtitle>
-
-        <v-card-title class="text-h4 mb-4">
-          <span class="text-primary">{{ form.total_price }}</span> {{ $t('DA') }}
-        </v-card-title>
-
-        <v-card-text class="d-flex flex-column">
-          <v-number-input
-            class="orderline-input"
-            density="compact"
-            max-width="160"
-            variant="underlined"
-            :label="$t('payment')"
-            :max="form.total_price"
-            v-model="paymentForm.amount"
-          />
-
-          <v-number-input
-            class="orderline-input"
-            density="compact"
-            max-width="160"
-            variant="underlined"
-            :label="$t('reduction')"
-            :max="form.total_price"
-            v-model="form.reduction"
-          />
-          <v-chip v-if="toPay > 0" variant="text" size="small" color="warning">
-            {{ $t('remaining') }}: {{ toPay }} {{ $t('DA') }}
-          </v-chip>
-          <v-divider class="my-4"></v-divider>
-        </v-card-text>
-
-        <v-card-actions class="flex-column justify-end ga-2">
-          <v-btn
-            color="primary"
-            variant="flat"
+            variant="text"
+            :prepend-icon="mdiClockOutline"
+            color="info"
+            @click="saveSale"
             rounded
-            block
-            @click="submitSale"
-            :loading="isLoading"
-          >
-            {{ $t('confirm') }}
-          </v-btn>
-
-          <v-btn color="secondary" variant="tonal" rounded block @click="handleReset">
-            {{ $t('reset') }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-
-    <v-col v-if="showScanner" cols="12">
-      <v-card>
-        <v-card-text>
-          <BarcodeScanner @detected="(v) => selectProduct(v, orderlinesForm)" />
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
-
-  <v-row>
-    <v-col>
-      <!-- FOOTER ACTIONS -->
-      <div class="d-flex justify-space-between">
-        <div class="d-flex ga-2 flex-wrap">
-          <v-chip
-            v-for="(_, i) in savedSales"
-            :key="i"
-            @click="restoreSale(i)"
+            density="compact"
             size="small"
-            color="secondary"
-            variant="tonal"
           >
-            {{ $t('sale') }} #{{ i }}
-          </v-chip>
+            {{ $t('save-for-later') }}</v-btn
+          >
         </div>
-        <v-btn
-          variant="text"
-          :prepend-icon="mdiClockOutline"
-          color="info"
-          @click="saveSale"
-          rounded
-          density="compact"
-          size="small"
-        >
-          {{ $t('save-for-later') }}</v-btn
-        >
-      </div>
-    </v-col>
-  </v-row>
-  <v-divider class="mt-8 mb-16" />
-  <OrdersTable :orders="orders">
-    <template #title>
-      <div class="d-flex justify-space-between align-center">
-        <div class="text-h5 pa-4">{{ $t('sales-list') }}</div>
-      </div>
-    </template>
-    <template #top>
-      <div class="d-flex align-center justify-space-between flex-wrap ga-4">
-        <FilterBar class="flex-grow-1" v-model="filters" />
+      </v-col>
+    </v-row>
+    <v-divider class="mt-8 mb-16" />
+    <OrdersTable :orders="orders">
+      <template #title>
+        <div class="d-flex justify-space-between align-center">
+          <div class="text-h5 pa-4">{{ $t('sales-list') }}</div>
+        </div>
+      </template>
+      <template #top>
+        <div class="d-flex align-center justify-space-between flex-wrap ga-4">
+          <FilterBar class="flex-grow-1" v-model="filters" />
 
-        <div class="d-flex align-center ga-2">
-          <v-chip size="small" color="primary" variant="tonal">
-            {{ ordersCount }}
-          </v-chip>
-          <v-chip size="small" color="secondary" variant="tonal">
-            {{ $t('total') }}: {{ ordersTotal }} {{ $t('DA') }}
-          </v-chip>
+          <div class="d-flex align-center ga-2">
+            <v-chip size="small" color="primary" variant="tonal">
+              {{ ordersCount }}
+            </v-chip>
+            <v-chip size="small" color="secondary" variant="tonal">
+              {{ $t('total') }}: {{ ordersTotal }} {{ $t('DA') }}
+            </v-chip>
+          </div>
         </div>
-      </div>
-    </template>
-  </OrdersTable>
+      </template>
+    </OrdersTable>
+  </v-card>
 </template>
 
 <script setup lang="ts">
@@ -370,6 +365,11 @@ function insertPayment(payment: TablesInsert<'payments'>) {
   ])
 }
 
+function updateProductQuantities(ids: string[]) {
+  updateProductsQtyDb.stockMovementsIds.value = ids
+  updateProductsQtyDb.execute()
+}
+
 function insertNotification(title: string, body: string) {
   upsertNotificationsDb.execute([
     {
@@ -380,11 +380,6 @@ function insertNotification(title: string, body: string) {
       _synced: false
     }
   ])
-}
-
-function updateProductQuantities(ids: string[]) {
-  updateProductsQtyDb.stockMovementsIds.value = ids
-  updateProductsQtyDb.execute()
 }
 
 function handleKeyDown(event: KeyboardEvent) {
@@ -414,7 +409,6 @@ onUnmounted(() => {
 upsertOrdersDb.onSuccess((data) => {
   const order = data[0]
   const order_id = order?.id
-
   if (order_id) {
     upsertOrderlinesDb.execute(
       orderlinesForm.value?.map((o) => ({
@@ -423,6 +417,7 @@ upsertOrdersDb.onSuccess((data) => {
         _synced: false
       }))
     )
+
     if (paymentForm.amount > 0) {
       insertPayment({ ...paymentForm, order_id })
     }
